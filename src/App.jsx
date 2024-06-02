e7dae06a7b3a58b25c8dc7dfb5be51958fb11092
Samadali123 <samadali0125@gmail.com>
2024-06-02 14:40:32 +0530

    learn topics basics to intermediate level

--- src/App.jsx
+++ src/App.jsx
@@ -19,97 +19,76 @@ import Routes from "./routes/Approutes"
 
 
 
-
-
-
-
 const App = () => {
 
-  const  [age1, setage1] = useState(0);
-  const  [age2, setage2] = useState(0);
-  const  [age3, setage3] = useState(0);
-  const  [age4, setage4] = useState(0);
-  const  [age5, setage5] = useState(0);
-  let age = 25;
-
-    // const getusers = async ()=>{
-    //   // use axios through then catch
-
-    //   //  const users = axios.get("https://jsonplaceholder.typicode.com/users").then(function(res){
-    //   //   console.log(res.data)
-    //   //  }).catch(function(error){
-    //   //   console.log(error)
-    //   //  })
-      
-
-    //   try {
-    //     const {data} = await  axios.get("/users")
-    //     console.log(data)
-    //   } catch (error) {
-    //     console.error(error)
-    //   }
-    
-    // }
-
-   
-  return (
-    <>
-   
-     <div className='w-[100vw] h-screen overflow-x-hidden overflow-y-auto bg-zinc-950 text-white flex justify-center flex-wrap  p-6 '>
-     <Routes/>
-     
-       {/* <Miniproject/> */}
-
-       {/* <Useeffect/>*/}
-
-       {/* <USERS/> */}
-       
-       {/* <button className='px-4 py-1 rounded-lg bg-orange-600 text-white' onClick={getusers}>Get users</button> */}
-      
-
-        
-     {/* <Cards/> */}
-     
-    
-      {/* <Ref/> */}
-
-
-      {/* <TwoWayBinding/> */}
-      
-      {/* <UpdateArrayofobjects/> */}
-
-
-      {/* <Updateobject/> */}
-
-        
-      {/* <Updatearr/> */}
-      
-     
-      {/* <Users/> */}
+    const [age1, setage1] = useState(0);
+    const [age2, setage2] = useState(0);
+    const [age3, setage3] = useState(0);
+    const [age4, setage4] = useState(0);
+    const [age5, setage5] = useState(0);
+    let age = 25;
 
 
 
-      {/* <h1>Conitional rendering in react js</h1>
-      <Conditional  age={age} /> */}
+
+    return ( <
+        >
+
+        <
+        div className = 'w-[100vw] h-screen overflow-x-hidden overflow-y-auto bg-zinc-950 text-white flex justify-center flex-wrap  p-6 ' > { /* <Routes/> */ }
+
+        { /* <Miniproject/> */ }
+
+        { /* <Useeffect/>*/ }
+
+        { /* <USERS/> */ }
+
+        { /* <button className='px-4 py-1 rounded-lg bg-orange-600 text-white' onClick={getusers}>Get users</button> */ }
+
+        { /* <Cards/> */ }
+
+
+        { /* <Ref/> */ }
+
+
+        { /* <TwoWayBinding/> */ }
+
+        { /* <UpdateArrayofobjects/> */ }
+
+
+        { /* <Updateobject/> */ }
+
+
+        { /* <Updatearr/> */ }
+
+
+        { /* <Users/> */ }
 
 
 
-     {/* <Card  name="Ahsan Ahemd" age={age1} setage={setage1} image="https://images.unsplash.com/photo-1715412406818-48241c841c9e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      <Card  name="syed samad ali" age={age2} setage={setage2} image="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      <Card  name="Arham ali" age={age3} setage={setage3} image="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      <Card  name="salim khan" age={age4} setage={setage4} image="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-       */}
+        {
+            /* <h1>Conitional rendering in react js</h1>
+                  <Conditional  age={age} /> */
+        }
 
 
-     </div>
-    </>
-  )
+
+        {
+            /* <Card  name="Ahsan Ahemd" age={age1} setage={setage1} image="https://images.unsplash.com/photo-1715412406818-48241c841c9e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  <Card  name="syed samad ali" age={age2} setage={setage2} image="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  <Card  name="Arham ali" age={age3} setage={setage3} image="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  <Card  name="salim khan" age={age4} setage={setage4} image="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                   */
+        }
+
+
+        <
+        /div> <
+        />
+    )
 }
 
 
 
 
 export default App
-
-
-
